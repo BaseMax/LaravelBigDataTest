ee3a9be0c4d4df29863f9afdaa5aba3da08c72fb
muuhab <mohab.barca@gmail.com>
2021-07-15 19:02:56 +0200

    FetchUsers(Custome command) added

---
+++ app/Console/Commands/FetchUsers.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Controllers\MailController;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class FetchUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:fetch';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $users=User::all();
+        $mail=(new MailController())->sendMail();
+        return 0;
+    }
+}
